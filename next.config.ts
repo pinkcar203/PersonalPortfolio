ddb4fedf50207bfb1dc84cd5cb10b5914ebda68e
pinkcar203 <prithikashrikannan@gmail.com>
2026-04-05 16:14:59 +0800

    fix basepath

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
   images: { unoptimized: true },
+  basePath: '/PersonalPortfolio',
+  assetPrefix: '/PersonalPortfolio',
 }
 
 export default nextConfig
